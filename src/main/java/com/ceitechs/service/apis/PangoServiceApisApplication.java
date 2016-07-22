53fe127c8ff57ce7a742c3b1f37a38d3cac8eb9e
Iddy Magohe <iddyiam@gmail.com>
2016-07-21 21:22:29 -0400

    initial

---
+++ src/main/java/com/ceitechs/service/apis/PangoServiceApisApplication.java
@@ -0,0 +1,12 @@
+package com.ceitechs.service.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangoServiceApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangoServiceApisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ceitechs/service/apis/PangoServiceApisApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ceitechs.service.apis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PangoServiceApisApplication.class)
+@WebAppConfiguration
+public class PangoServiceApisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
