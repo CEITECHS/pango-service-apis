83f3a1949163598760d840878097b6627d5125d5
Iddy Magohe <iddyiam@gmail.com>
2016-07-21 22:12:32 -0400

    initial config necessary to get pango-rentals-services up

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # pango-service-apis
 pango services endpoints
 - Run config-server
+ 
+ 
+# TODOS
+ 1. Enable Discovery Server - to allow service registration.
--- src/main/java/com/ceitechs/service/apis/PangoServiceApisApplication.java
+++ src/main/java/com/ceitechs/service/apis/PangoServiceApisApplication.java
@@ -1,7 +1,18 @@
 package com.ceitechs.service.apis;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class PangoServiceApisApplication {
@@ -9,4 +20,32 @@ public class PangoServiceApisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PangoServiceApisApplication.class, args);
 	}
+}
+
+@Getter
+@Setter
+class Rental {
+
+    private Long id;
+    private String reservationName;
+
+    public Rental(String reservationName) {
+        this.reservationName = reservationName;
+        this.id = System.currentTimeMillis();
+    }
+}
+
+@RefreshScope
+@RestController
+class ReservationRestController{
+
+	@Value("${pango.tagline}")
+	private String tagline;
+
+	@RequestMapping(value = "/rentals", method = RequestMethod.GET)
+    Collection<Rental> getRentals(){
+		return Arrays.asList(tagline.split(",")).stream().map(Rental::new).collect(Collectors.toList());
+
+	}
+
 }
