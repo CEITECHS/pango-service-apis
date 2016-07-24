48d38176b5da650fa38ea009fa67c7b7b34eec2c
Iddy Magohe <iddyiam@gmail.com>
2016-07-24 15:19:29 -0400

    updated readme

--- src/main/java/com/ceitechs/service/apis/PangoServiceApisApplication.java
+++ src/main/java/com/ceitechs/service/apis/PangoServiceApisApplication.java
@@ -1,13 +1,21 @@
 package com.ceitechs.service.apis;
 
+import com.ceitechs.domain.autoconfigure.EnablePangoDomainService;
+import com.ceitechs.domain.service.service.PangoDomainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnablePangoDomainService
 public class PangoServiceApisApplication {
+
+    @Autowired
+    PangoDomainService pangoDomainService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PangoServiceApisApplication.class, args);
 	}
 }
+
 
